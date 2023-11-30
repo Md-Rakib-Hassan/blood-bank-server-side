76fb265844a0799b04f5aa4a5a442a3520084c25
Md-Rakib-Hassan <md.rakibhassan4591@gmail.com>
2023-11-30 23:38:58 +0600

    Remove the db

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 
 // middleware
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['https://blood-bank-ee717.web.app'],
   credentials: true,
   optionSuccessStatus: 200
 }));
@@ -34,7 +34,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const dataBase = client.db('BloodBank');
 
@@ -162,7 +162,7 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
