0db8c6d247951aa241e7dac1dc4ae528a99f2a92
Md-Rakib-Hassan <md.rakibhassan4591@gmail.com>
2023-12-04 19:46:59 +0600

    made blog api delet api promoting api etc

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 
 // middleware
 app.use(cors({
-  origin: ['https://blood-bank-ee717.web.app'],
+  origin: ['http://localhost:5173','https://blood-bank-ee717.web.app','https://spiffy-lollipop-bbb8a6.netlify.app'],
   credentials: true,
   optionSuccessStatus: 200
 }));
@@ -34,7 +34,6 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
 
     const dataBase = client.db('BloodBank');
 
@@ -47,9 +46,23 @@ async function run() {
       res.send(result);
     })
 
+    app.post('/api/v1/donetion-req', async function (req, res) {
+      const Collection = dataBase.collection("donetionReq");
+      const Info = req.body;
+      const result = await Collection.insertOne(Info);
+      res.send(result);
+    })
+
+    app.post('/api/v1/blogs', async function (req, res) {
+      const Collection = dataBase.collection("blogs");
+      const Info = req.body;
+      const result = await Collection.insertOne(Info);
+      res.send(result);
+    })
+
     app.get('/api/v1/search-doner', async (req, res) => {
       const donorsCollection = dataBase.collection('users');
-      let { blood_group, state, city, email,role } = req.query;
+      let { blood_group, state, city, email, role } = req.query;
 
       if (blood_group == 'A ') blood_group = 'A+'
       else if (blood_group == 'B ') blood_group = 'B+'
@@ -57,8 +70,8 @@ async function run() {
       else if (blood_group == 'AB ') blood_group = 'AB+'
 
       // Build the query based on the provided parameters
-      let query = {role:role};
-
+      let query = {};
+      if (role) query['role'] = role;
       if (blood_group) {
         query['blood_group'] = blood_group;
       }
@@ -92,6 +105,31 @@ async function run() {
     });
 
 
+    app.get('/api/v1/donation-req', async (req, res) => {
+      const donorsCollection = dataBase.collection('donetionReq');
+      let { status,requesterEmail,id } = req.query;
+
+
+      // Build the query based on the provided parameters
+      let query = {};
+      if (status) query['status'] = status;
+      if (requesterEmail) {
+        query['requesterEmail'] = requesterEmail;
+      }
+      if(id) query['_id'] =new ObjectId(id);
+
+
+      try {
+        const donors = await donorsCollection.find(query).toArray();
+        res.send(donors);
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+      }
+
+    });
+
+
 
     app.get('/api/v1/user/:email', async (req, res) => {
       console.log(req.params.email);
@@ -101,6 +139,28 @@ async function run() {
       res.send(result);
 
     })
+
+    app.get('/api/v1/blogs', async (req, res) => {
+      const {status}=req.query;
+      let query={};
+      if(status)query['status']=status;
+      const Collection = dataBase.collection("blogs");
+      const coursor = Collection.find(query);
+      const result = await coursor.toArray();
+      res.send(result);
+
+    })
+
+    app.get('/api/v1/blogs/:id', async (req, res) => {
+      const id=req.params.id;
+      const Collection = dataBase.collection("blogs");
+      const result = await Collection.findOne({_id: new ObjectId(id)});
+      res.send(result);
+
+    })
+  
+
+    
 
     app.get('/api/v1/districts', async (req, res) => {
       console.log(req.params.email);
@@ -123,38 +183,162 @@ async function run() {
     app.get('/api/v1/upazilas/:district_id', async (req, res) => {
       console.log(req.params.email);
       const Collection = dataBase.collection("upazilas");
-      const coursor = Collection.find({district_id:`${req.params.district_id}`})
+      const coursor = Collection.find({ district_id: `${req.params.district_id}` })
       const result = await coursor.toArray();
       res.send(result);
 
     })
 
-    app.put('/api/v1/update-profile/:id',async (req, res) => {
-      const id=req.params.id;
-      const filter={_id: new ObjectId(id)}
-      const options= {upsert: true};
+    app.put('/api/v1/update-profile/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const options = { upsert: true };
       const Collection = dataBase.collection("users");
-      const updatedProfile=req.body;
+      const updatedProfile = req.body;
 
-      const user={
-          
+      const user = {
+
 
         $set: {
           name: updatedProfile.name,
           blood_group: updatedProfile.blood_group,
           district: updatedProfile.district,
           upazila: updatedProfile.upazila,
-          
+
         },
       }
 
-      const result= await Collection.updateOne(filter,user,options);
+      const result = await Collection.updateOne(filter, user, options);
       res.send(result);
 
 
     })
 
+    app.put('/api/v1/update-role/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const options = { upsert: true };
+      const Collection = dataBase.collection("users");
+      const {role} = req.body;
 
+      const user = {
+
+
+        $set: {
+          role: role,
+        },
+      }
+
+      const result = await Collection.updateOne(filter, user, options);
+      res.send(result);
+
+
+    })
+
+    app.put('/api/v1/status/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const options = { upsert: true };
+      const Collection = dataBase.collection("users");
+      const {status} = req.body;
+
+      const user = {
+
+
+        $set: {
+          status: status,
+        },
+      }
+
+      const result = await Collection.updateOne(filter, user, options);
+      res.send(result);
+
+
+    })
+
+    app.delete('/api/v1/blogs/delete/:id', async (req, res) => {
+      const id = req.params.id;
+      const Collection = dataBase.collection("blogs");
+      const query={_id: new ObjectId(id)}
+      const result= await Collection.deleteOne(query);
+      res.send(result);
+
+    });
+
+    app.delete('/api/v1/users/delete/:id', async (req, res) => {
+      const id = req.params.id;
+      const Collection = dataBase.collection("users");
+      const query={_id: new ObjectId(id)}
+      const result= await Collection.deleteOne(query);
+      res.send(result);
+
+    });
+
+    app.delete('/api/v1/donation-requests/delete/:id', async (req, res) => {
+      const id = req.params.id;
+      const Collection = dataBase.collection("donetionReq");
+      const query={_id: new ObjectId(id)}
+      const result= await Collection.deleteOne(query);
+      res.send(result);
+
+    });
+
+    app.put('/api/v1/blogs/publish/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const options = { upsert: true };
+      const Collection = dataBase.collection("blogs");
+      const updatedBlog = req.body;
+
+      const blog = {
+
+
+        $set: {
+          status: 'published',
+        },
+      }
+
+      const result = await Collection.updateOne(filter, blog, options);
+      res.send(result);
+
+
+    })
+
+    app.put('/api/v1/update/donetion-req/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const options = { upsert: true };
+      const Collection = dataBase.collection("donetionReq");
+      const {status} = req.body;
+
+      const up = {
+
+
+        $set: {
+          status: status,
+        },
+      }
+
+      const result = await Collection.updateOne(filter, up, options);
+      res.send(result);
+
+
+    })
+
+    app.post('/api/v1/contacts-info', async function (req, res) {
+      const contactCollection = dataBase.collection("contactsInfo");
+      const ContactInfo = req.body;
+      const result = await contactCollection.insertOne(ContactInfo);
+      res.send(result);
+    })
+
+
+    app.post('/api/v1/subscribers', async function (req, res) {
+      const Collection = dataBase.collection("subscribers");
+      const ReqInfo = req.body;
+      const result = await Collection.insertOne(ReqInfo);
+      res.send(result);
+    })
 
 
 
@@ -162,7 +346,7 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
+    await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
